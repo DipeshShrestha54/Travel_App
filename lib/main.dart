3be1ee53095d00f721394053b9d4ea143781e22a
Dipesh Shrestha <shrestha.dipesh5454@gmail.com>
2022-12-11 22:11:03 +0545

    ui code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/pages/navpages/main_page.dart';
 import 'package:travel_app/pages/welcome_page.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: "Travel App",
     debugShowCheckedModeBanner: false,
-    home: WelcomePage(),
+    home: MainPage(),
   ));
 }
